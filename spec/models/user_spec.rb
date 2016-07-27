9b15019328fd5edc4f0f6e24009ec61cda9e7e90
MpaK <mrak69@gmail.com>
2016-07-27 21:35:47 +0700

    User model specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,29 @@
+RSpec.describe User, type: :model do
+  subject { FactoryGirl.build(:user) }
+
+  context 'with validation' do
+    it 'checks empty name' do
+      subject.name = nil
+      subject.valid?
+      expect(subject.errors[:first_name].count).to be > 0
+    end
+
+    it 'check length of name' do
+      subject.name = 'Astrum' * 100
+      subject.valid?
+      expect(subject.errors[:name].count).to be > 0
+    end
+
+    it 'checks empty email' do
+      subject.email = nil
+      subject.valid?
+      expect(subject.errors[:last_name].count).to be > 0
+    end
+
+    it 'checks uniqueness of email' do
+      create(:user, first_name: subject.first_name, last_name: subject.last_name)
+      subject.valid?
+      expect(subject.errors[:full_name].count).to be > 0
+    end
+  end
+end
