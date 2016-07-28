82f4948168230f5270d9627a5f124a88fc9473d3
stiig <vasiliyrps2@gmail.com>
2016-07-28 21:06:30 +0500

    Added registration by Devise

---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+  validates :name, presence: true, allow_blank: false, length: {maximum: 255}
+  validates :email, presence: true, allow_blank: false
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  created_at             :datetime         not null
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string(255)
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  id                     :integer          not null, primary key
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string(255)
+#  name                   :string(255)      not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string(255)
+#  sign_in_count          :integer          default(0), not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
---
+++ spec/factories/user.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    email "aabb@hh.de"
+    name "sad"
+    password "123456"
+    password_confirmation "123456"
+  end
+end
