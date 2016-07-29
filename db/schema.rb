6004013e5afeab7fedeb6549725a09c7241eb92a
stiig <vasiliyrps2@gmail.com>
2016-07-29 21:47:01 +0500

    Deleted unnecessary files and json from respond

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20160729144906) do
 
   create_table "companies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
+    t.string   "name",                      null: false
     t.text     "description", limit: 65535
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
