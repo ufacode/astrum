8f3f327781f632766549d5703d88084c494fdde4
mpakus <mrak69@gmail.com>
2018-08-18 21:16:33 +0700

    Seeds user; Cleanup companies form;

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true
-  # Include default devise modules. Others available are:
+
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
